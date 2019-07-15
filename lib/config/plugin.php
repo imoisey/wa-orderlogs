0c06e854f85d3cee1b9680afbda7284f7939370f
Igor Moiseev <developer.modx@gmail.com>
2019-07-15 21:33:07 +0300

    Update version

--- lib/config/plugin.php
+++ lib/config/plugin.php
@@ -3,7 +3,7 @@
 return array(
 	'name'			=> 'История изменений заказа',
 	'description'	=> 'Собирает историю изменений в заказе и сохраняет ее в логи',
-	'version'		=> '1.6.0',
+	'version'		=> '1.6.1',
 	'vendor'		=> 1028182,
 	'handlers'		=> array(
 		'backend_order_edit'	=> 'backendOrderlogs',
